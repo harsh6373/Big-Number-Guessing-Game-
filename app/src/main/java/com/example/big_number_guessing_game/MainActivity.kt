0c4d82c508da4d83b297526d1439fcecc94931ba
Harsh Jani <harshjani6373@gmail.com>
2022-11-20 14:56:54 +0530

    initial commit

---
+++ app/src/main/java/com/example/big_number_guessing_game/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.big_number_guessing_game
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.github.jinatonic.confetti.CommonConfetti
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //val txt_score : TextView = findViewById(R.id.txt_score)
+
+        leftButton.setOnClickListener {
+
+            checkAnswer(true)
+            assignnewnumbers()
+
+        }
+
+
+        rightButton.setOnClickListener {
+
+            checkAnswer(false)
+            assignnewnumbers()
+
+        }
+
+    }
+
+    private fun checkAnswer(isleftBtnSelected: Boolean) {
+
+        val left_num: Int = leftButton.text.toString().toInt()
+        val right_num: Int = rightButton.text.toString().toInt()
+        var answer = if (isleftBtnSelected) left_num > right_num else left_num < right_num
+
+        if (answer) {
+            constraintlayout.setBackgroundColor(Color.GREEN)
+            Toast.makeText(this, "Correct!!", Toast.LENGTH_SHORT).show()
+            counter = counter + 1
+        } else {
+            constraintlayout.setBackgroundColor(Color.RED)
+            Toast.makeText(this, "Wrong..", Toast.LENGTH_SHORT).show()
+            counter = 0
+        }
+
+        val centerX: Int = constraintlayout.width / 2
+        val centerY: Int = constraintlayout.height / 5 * 2
+
+
+
+        txt_score.text = counter.toString()
+
+    }
+
+    private fun assignnewnumbers() {
+        val random_num = Random()
+
+        val left_num: Int = random_num.nextInt(10)
+        var right_num: Int = left_num
+
+        while (right_num == left_num) {
+            right_num = random_num.nextInt(10)
+        }
+        leftButton.text = left_num.toString()
+        rightButton.text = right_num.toString()
+    }
+
+
+}
